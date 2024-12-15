a1c04e6c94140be8af2c027b6778b99f61a0b2cd
Bishalkl <Bishal123koirala@gmail.com>
2024-12-15 15:48:41 +0545

    done

---
+++ Convert/binToDec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// creating a function to conver the binary to decimal
+int convertBinToDecimal(int number) {
+    int power = 0, totalDec = 0;
+    while (number > 0) {
+        totalDec += (number % 10) * (1 << power);
+        number /= 10;
+        power++;
+    }
+    return totalDec;
+}
+
+int main() {
+    int binNumber;
+    // Taking the input value 
+    cout << "Enter the binary number: ";
+    cin >> binNumber;
+
+    cout << "Converted decimal value is " << convertBinToDecimal(binNumber) << endl;
+    return 0;
+}
